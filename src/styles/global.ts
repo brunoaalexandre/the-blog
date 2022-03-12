a9d011f0ad440ef0cb95acebf03a138a1a120c02
Bruno Alexandre <brunoalencaralexandre@gmail.com>
2022-03-12 16:17:02 -0300

    Set the color theme

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,7 +2,25 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   :root {
+    --black: "#000000";
+    --gray: "#BBBBBB";
+    --white: "#FFFFFF";
 
+    --green: "#4FFF4B";
+
+    --purple-100: "#FBF6FF";
+    --purple-90: "#F2E7FA"
+    --purple-50: "#9E6DC2";
+    --purple-30: "#290742";
+    --purple-10: "#170027";
+
+    --purple-gradient: gradient(
+        104.9deg,
+        rgba(239, 35, 243, 0.7) -2.03%,
+        rgba(195, 38, 228, 0.7) 27.04%,
+        rgba(139, 42, 209, 0.7) 67.97%,
+        rgba(92, 44, 193, 0.7) 107.39%
+        );
   }
 
   * {
