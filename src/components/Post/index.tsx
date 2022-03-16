70c8f4b904f3e151a962a7619333f35409af3238
Bruno Alexandre <brunoalencaralexandre@gmail.com>
2022-03-15 23:32:53 -0300

    Create post component

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,22 +1,23 @@
 import { Container } from "./styles";
 
-import img01 from "../../assets/img_01.png";
+interface PostProps {
+  img: string;
+  title: string;
+  date: string;
+  text: string;
+}
 
-export function Post() {
+export function Post({ img, title, date, text }: PostProps) {
   return (
     <Container>
       <header>
         <div>
-          <img src={img01} alt="10 dicas para conseguir a vaga desejada" />
+          <img src={img} alt="" />
         </div>
-        <span>Janeiro 04, 2022</span>
+        <span>{date}</span>
       </header>
-      <h1>10 dicas para conseguir a vaga desejada</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh nibh eu in
-        aliquet ut adipiscing neque. Sed volutpat aenean sit vitae, sed
-        tristique.
-      </p>
+      <h1>{title}</h1>
+      <p>{text}</p>
     </Container>
   );
 }
--- src/components/Post/styles.ts
+++ src/components/Post/styles.ts
@@ -1,21 +1,45 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  font-family: "Poppins", sans-serif;
+  max-width: 22.125rem;
+  font-family: "Roboto", sans-serif;
 
   header {
     div {
+      height: 16.25rem;
       background: linear-gradient(
         104.9deg,
-        rgba(239, 35, 243, 0.5) -2.03%,
-        rgba(195, 38, 228, 0.5) 27.04%,
-        rgba(139, 42, 209, 0.5) 67.97%,
-        rgba(92, 44, 193, 0.5) 107.39%
+        rgba(239, 35, 243, 0.7) -2.03%,
+        rgba(195, 38, 228, 0.7) 27.04%,
+        rgba(139, 42, 209, 0.7) 67.97%,
+        rgba(92, 44, 193, 0.7) 107.39%
       );
+      border-radius: 0.5rem;
 
       img {
+        width: 100%;
+        height: 16.25rem;
         opacity: 0.5;
       }
     }
+
+    span {
+      display: inline-block;
+      font-size: 1rem;
+      color: var(--black);
+      margin-top: 1rem;
+    }
+  }
+
+  h1 {
+    font-size: 1.5rem;
+    line-height: 1.5rem;
+    margin-top: 0.5rem;
+  }
+
+  p {
+    font-size: 1rem;
+    line-height: 1.5rem;
+    margin-top: 0.5rem;
   }
 `;
